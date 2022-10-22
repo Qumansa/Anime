73f83535d9373b3aa3095bb788e56e5f4521748c
Qumansa <qumansa@yandex.ru>
2022-10-22 17:10:28 +0300

    Исправление названий папок и компонентов

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,38 +1,44 @@
-import { Link, NavLink } from 'react-router-dom'; 
+import { Link, NavLink } from 'react-router-dom';
 
-import './appHeader.sass';
+import './appheader.sass';
 import logo from '../../resources/img/logo.svg';
 
 const AppHeader = () => {
-    return (
-        <header className="header">
-            <div className="header__container container">
-                <Link to="/" className="header__logo-link">
-                    <img className="header__logo-img" src={logo} alt="Studio Ghibli"/>
-                </Link>
-                <nav className="header__nav">
-                    <ul className="header__list">
-                        <li className="header__item">
-                            <NavLink 
-                                className="header__item-link"
-                                style={({ isActive }) => ({color: isActive ? '#317af9' : 'inherit'})}
-                                to="/films">
-                                Films
-                            </NavLink>
-                        </li>
-                        <li className="header__item">
-                            <NavLink 
-                                className="header__item-link"
-                                style={({ isActive }) => ({color: isActive ? '#317af9' : 'inherit'})}
-                                to="/characters">
-                                Characters
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
+	return (
+		<header className="header">
+			<div className="header__container container">
+				<Link
+					to="/"
+					className="header__logo-link">
+					<img
+						className="header__logo-img"
+						src={logo}
+						alt="Studio Ghibli"
+					/>
+				</Link>
+				<nav className="header__nav">
+					<ul className="header__list">
+						<li className="header__item">
+							<NavLink
+								className="header__item-link"
+								style={({ isActive }) => ({ color: isActive ? '#317af9' : 'inherit' })}
+								to="/films">
+								Films
+							</NavLink>
+						</li>
+						<li className="header__item">
+							<NavLink
+								className="header__item-link"
+								style={({ isActive }) => ({ color: isActive ? '#317af9' : 'inherit' })}
+								to="/characters">
+								Characters
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default AppHeader;
--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 import useGhibliService from '../../service/GhibliService';
 import Spinner from '../spinner/Spinner';
@@ -10,81 +10,90 @@ import getFilmDuration from '../../utils/getFilmDuration';
 import './film.sass';
 
 const Film = () => {
-    const {error, getFilm, loading} = useGhibliService();
-    const [film, setFilm] = useState({});
-    const {id} = useParams();
+	const { error, getFilm, loading } = useGhibliService();
+	const { id } = useParams();
+	const [film, setFilm] = useState({});
 
-    const onFilmLoaded = (film) => {
-        setFilm(film);
-    };
+	const onFilmLoaded = (film) => {
+		setFilm(film);
+	};
 
-    const updateFilm = () => {
-        getFilm(id)
-            .then(onFilmLoaded);
-    };
+	const updateFilm = () => {
+		getFilm(id).then(onFilmLoaded);
+	};
 
-    useEffect(() => {
-        updateFilm();
-    }, [id]);
+	useEffect(() => {
+		updateFilm();
+	}, [id]);
 
-    const {description, director, image, originalTitle, producer, rating, releaseDate, runningTime, title} = film;
-    
-    const duration = getFilmDuration(runningTime);
+	const { description, director, image, originalTitle, producer, rating, releaseDate, runningTime, title } = film;
 
-    const View = () => {
-        return (
-            <div className="film__wrapper">
-                <img src={image} alt={title} className="film__img"/>
-                <div className="film__main">
-                    <div className="film__header">
-                        <h2 className="film__title">
-                            <span className="film__title-text film__title-text_eng">{title}</span>
-                            <span className="film__title-text film__title-text_jp">{originalTitle}</span>
-                        </h2>
-                        <Link className="film__back" to="/films">Back to all</Link>
-                    </div>
-                    <ul className="film__info">
-                        <li className="film__info-item">
-                            <span className="film__text">Producer: </span>
-                            <span className="film__value">{producer}</span>
-                        </li>
-                        <li className="film__info-item">
-                            <span className="film__text">Director: </span>
-                            <span className="film__value">{director}</span>
-                        </li>
-                        <li className="film__info-item">
-                            <span className="film__text">Released in: </span>
-                            <span className="film__value">{releaseDate}</span>
-                        </li>
-                        <li className="film__info-item">
-                            <span className="film__text">Duration: </span>
-                            <span className="film__value">{runningTime} min./{duration}</span>
-                        </li>
-                        <li className="film__info-item">
-                            <span className="film__text">Rating: </span>
-                            <span className="film__value">{rating}/100</span>
-                        </li>
-                    </ul>
-                    <div className="film__descr">{description}</div>
-                </div>
-            </div>
-        );
-    };
+	const duration = getFilmDuration(runningTime);
 
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading) && !(error) ? <View/> : null;
+	const View = () => {
+		return (
+			<div className="film__wrapper">
+				<img
+					src={image}
+					alt={title}
+					className="film__img"
+				/>
+				<div className="film__main">
+					<div className="film__header">
+						<h2 className="film__title">
+							<span className="film__title-text film__title-text_eng">{title}</span>
+							<span className="film__title-text film__title-text_jp">{originalTitle}</span>
+						</h2>
+						<Link
+							className="film__back"
+							to="/films">
+							Back to all
+						</Link>
+					</div>
+					<ul className="film__info">
+						<li className="film__info-item">
+							<span className="film__text">Producer: </span>
+							<span className="film__value">{producer}</span>
+						</li>
+						<li className="film__info-item">
+							<span className="film__text">Director: </span>
+							<span className="film__value">{director}</span>
+						</li>
+						<li className="film__info-item">
+							<span className="film__text">Released in: </span>
+							<span className="film__value">{releaseDate}</span>
+						</li>
+						<li className="film__info-item">
+							<span className="film__text">Duration: </span>
+							<span className="film__value">
+								{runningTime} min./{duration}
+							</span>
+						</li>
+						<li className="film__info-item">
+							<span className="film__text">Rating: </span>
+							<span className="film__value">{rating}/100</span>
+						</li>
+					</ul>
+					<div className="film__descr">{description}</div>
+				</div>
+			</div>
+		);
+	};
 
-    return (
-        <section className="film section last-section">
-            <h1 className="sr-only">{title}</h1>
-            <div className="container">
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        </section>
-    );
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
+	const content = !loading && !error ? <View /> : null;
+
+	return (
+		<section className="film section last-section">
+			<h1 className="sr-only">{title}</h1>
+			<div className="container">
+				{errorMessage}
+				{spinner}
+				{content}
+			</div>
+		</section>
+	);
 };
 
 export default Film;
