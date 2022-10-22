61cbd3229ddf3d601c90fef01a2d7b21977937c8
Qumansa <qumansa@yandex.ru>
2022-10-22 16:42:38 +0300

    Мелкие правки перед деплоем на Vercel

--- src/components/films/Films.js
+++ src/components/films/Films.js
@@ -8,65 +8,66 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import './films.sass';
 
 const Films = () => {
-    const {error, getFilms, loading, setLoading} = useGhibliService();
-    const [films, setFilms] = useState([]);
+	const { error, getFilms, loading, setLoading } = useGhibliService();
+	const [films, setFilms] = useState([]);
 
-    const onFilmsLoaded = (newFilms) => {
-        setFilms(newFilms);
-        setLoading(false);
-    };
+	const onFilmsLoaded = (newFilms) => {
+		setFilms(newFilms);
+		setLoading(false);
+	};
 
-    const updateList = () => {
-        getFilms()
-            .then(onFilmsLoaded);
-    };
+	const updateList = () => {
+		getFilms().then(onFilmsLoaded);
+	};
 
-    useEffect(() => { 
-        updateList();
-    }, []);
-    
-    const renderList = (arr) => {
-        const items = arr.map((film) => {
-            const {id, image, originalTitle, title} = film;
+	useEffect(() => {
+		updateList();
+	}, []);
 
-            return (
-                <li 
-                    className="films__item"
-                    key={id}>
-                    <Link to={`/films/${id}`} className="films__link">
-                        <img src={image} alt={title} className="films__img"/>
-                        <h3 className="films__title">
-                            <span className="films__title-text films__title-text_eng">{title}</span>
-                            <span className="films__title-text films__title-text_jp">{originalTitle}</span>
-                        </h3>
-                    </Link>
-                </li>
-            );
-        });
+	const renderList = (arr) => {
+		const items = arr.map((film) => {
+			const { id, image, originalTitle, title } = film;
 
-        return (
-            <ul className="films__list">
-                {items}
-            </ul>
-        );
-    };
+			return (
+				<li
+					className="films__item"
+					key={id}>
+					<Link
+						to={`/films/${id}`}
+						className="films__link">
+						<img
+							src={image}
+							alt={title}
+							className="films__img"
+						/>
+						<h3 className="films__title">
+							<span className="films__title-text films__title-text_eng">{title}</span>
+							<span className="films__title-text films__title-text_jp">{originalTitle}</span>
+						</h3>
+					</Link>
+				</li>
+			);
+		});
 
-    const list = renderList(films);
+		return <ul className="films__list">{items}</ul>;
+	};
 
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading) ? list : null;
+	const list = renderList(films);
 
-    return (
-        <section className="films section">
-            <h2 className="sr-only">Films</h2>
-            <div className="container">
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        </section>
-    );
-}
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
+	const content = !loading ? list : null;
+
+	return (
+		<section className="films section">
+			<h2 className="sr-only">Films</h2>
+			<div className="container">
+				{errorMessage}
+				{spinner}
+				{content}
+			</div>
+		</section>
+	);
+};
 
 export default Films;
--- src/service/GhibliService.js
+++ src/service/GhibliService.js
@@ -1,93 +1,93 @@
-import useHttp from "../hooks/http.hook";
+import useHttp from '../hooks/http.hook';
 
 const useGhibliService = () => {
-    const {error, loading, request, setLoading} = useHttp();
+	const { error, loading, request, setLoading } = useHttp();
 
-    const _apiBase = 'https://ghibliapi.herokuapp.com/';
+	const _apiBase = 'https://ghibliapi.herokuapp.com/';
 
-    const getCharacter = async (id) => {
-        const res = await request({
-            url: `${_apiBase}people/${id}`,
-            isLoaded: false
-        });
-        
-        return transformCharacter(res);
-    };
+	const getCharacter = async (id) => {
+		const res = await request({
+			url: `${_apiBase}people/${id}`,
+			isLoaded: false,
+		});
 
-    const getCharacters = () => {
-        return Promise.all([
-            request({url: `${_apiBase}people/8228751c-bdc1-4b8d-a6eb-ca0eb909568f`}),
-            request({url: `${_apiBase}people/267649ac-fb1b-11eb-9a03-0242ac130003`}),
-            request({url: `${_apiBase}people/7151abc6-1a9e-4e6a-9711-ddb50ea572ec`}),
-            request({url: `${_apiBase}people/ebe40383-aad2-4208-90ab-698f00c581ab`}),
-            request({url: `${_apiBase}people/598f7048-74ff-41e0-92ef-87dc1ad980a9`}),
-            request({url: `${_apiBase}people/d39deecb-2bd0-4770-8b45-485f26e1381f`}),
-        ]);
-    };
+		return transformCharacter(res);
+	};
 
-    const getFilm = async (id) => {
-        const res = await request({url: `${_apiBase}films/${id}`});
-        
-        return transformFilm(res);
-    };
+	const getCharacters = () => {
+		return Promise.all([
+			request({ url: `${_apiBase}people/8228751c-bdc1-4b8d-a6eb-ca0eb909568f` }),
+			request({ url: `${_apiBase}people/267649ac-fb1b-11eb-9a03-0242ac130003` }),
+			request({ url: `${_apiBase}people/7151abc6-1a9e-4e6a-9711-ddb50ea572ec` }),
+			request({ url: `${_apiBase}people/ebe40383-aad2-4208-90ab-698f00c581ab` }),
+			request({ url: `${_apiBase}people/598f7048-74ff-41e0-92ef-87dc1ad980a9` }),
+			request({ url: `${_apiBase}people/d39deecb-2bd0-4770-8b45-485f26e1381f` }),
+		]);
+	};
 
-    const getFilms = async () => {
-        const res = await request({
-            url: `${_apiBase}films`,
-            isLoaded: false
-        });
-        
-        return res.map(transformFilms);
-    };
+	const getFilm = async (id) => {
+		const res = await request({ url: `${_apiBase}films/${id}` });
 
-    const transformFilm = (film) => {
-        return {
-            description: film.description,
-            director: film.director,
-            id: film.id,
-            image: film.image,
-            originalTitle: film.original_title,
-            originalTitleRomanised: film.original_title_romanised,
-            producer: film.producer,
-            releaseDate: film.release_date,
-            runningTime: film.running_time,
-            rating: film.rt_score,
-            title: film.title,
-        };
-    };
+		return transformFilm(res);
+	};
 
-    const transformFilms = (films) => {
-        return {
-            description: films.description, 
-            id: films.id,
-            image: films.image,
-            originalTitle: films.original_title,
-            originalTitleRomanised: films.original_title_romanised,
-            title: films.title,
-        };
-    };
+	const getFilms = async () => {
+		const res = await request({
+			url: `${_apiBase}films`,
+			isLoaded: false,
+		});
 
-    const transformCharacter = (character) => {
-        return {
-            age: character.age && character.age !== 'NA' ? character.age : 'Secret!',
-            eyeColor: character.eye_color,
-            films: character.films,
-            gender: character.gender && character.gender !== 'NA' ? character.gender : 'Secret!',
-            hairColor: character.hair_color,
-            id: character.id,
-            name: character.name
-        };
-    };
+		return res.map(transformFilms);
+	};
 
-    return {
-        error, 
-        getCharacter,
-        getCharacters,
-        getFilm,
-        getFilms,
-        loading, 
-        setLoading
-    };
+	const transformFilm = (film) => {
+		return {
+			description: film.description,
+			director: film.director,
+			id: film.id,
+			image: film.image,
+			originalTitle: film.original_title,
+			originalTitleRomanised: film.original_title_romanised,
+			producer: film.producer,
+			releaseDate: film.release_date,
+			runningTime: film.running_time,
+			rating: film.rt_score,
+			title: film.title,
+		};
+	};
+
+	const transformFilms = (films) => {
+		return {
+			description: films.description,
+			id: films.id,
+			image: films.image,
+			originalTitle: films.original_title,
+			originalTitleRomanised: films.original_title_romanised,
+			title: films.title,
+		};
+	};
+
+	const transformCharacter = (character) => {
+		return {
+			age: character.age && character.age !== 'NA' ? character.age : 'Secret!',
+			eyeColor: character.eye_color,
+			films: character.films,
+			gender: character.gender && character.gender !== 'NA' ? character.gender : 'Secret!',
+			hairColor: character.hair_color,
+			id: character.id,
+			name: character.name,
+		};
+	};
+
+	return {
+		error,
+		getCharacter,
+		getCharacters,
+		getFilm,
+		getFilms,
+		loading,
+		setLoading,
+	};
 };
 
 export default useGhibliService;
